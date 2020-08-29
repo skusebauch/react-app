5b17aec5ba6b5240b4b7efb2ecfdb1f404a79c4d
Sven Kusebauch <sven.kusebauch@gmail.com>
2020-08-29 17:35:48 +0200

    get rid of Radium and added Styled Components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
 import "./App.css";
-import Radium from "radium";
 import Person from "./Person/Person";
 
 class App extends Component {
@@ -105,4 +104,4 @@ class App extends Component {
   }
 }
 
-export default Radium(App);
+export default App;
