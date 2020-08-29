8959341c81715c8d26d4f1ad6b60186bf3f8220e
Sven Kusebauch <sven.kusebauch@gmail.com>
2020-08-29 19:18:16 +0200

    added css module

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 
-import "./App.css";
+import classes from "./App.css";
 import Person from "./Person/Person";
 
 //const StyledButton = styled.button`
@@ -59,6 +59,7 @@ class App extends Component {
 
   render() {
     let persons = null;
+    let btnClass = "";
 
     if (this.state.showPersons) {
       persons = (
@@ -76,23 +77,24 @@ class App extends Component {
           })}
         </div>
       );
+      btnClass = classes.Red;
     }
 
-    const classes = [];
+    const assignedClasses = [];
     if (this.state.persons.length <= 2) {
-      classes.push("red");
+      assignedClasses.push(classes.red);
     }
 
     if (this.state.persons.length <= 1) {
-      classes.push("bold");
+      assignedClasses.push(classes.bold);
     }
 
     return (
-      <div className="App">
+      <div className={classes.App}>
         <h1>Hi, I'm a React App</h1>
-        <p className={classes.join(" ")}>This is really working!</p>
+        <p className={assignedClasses.join(" ")}>This is really working!</p>
         <button
-          className="button"
+          className={btnClass}
           alt={this.state.showPersons}
           onClick={this.togglePersonsHandler}
         >
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -22,13 +22,13 @@ const person = (props) => {
   //};
   return (
     //<div className="Person" style={style}>
-    <StyledDiv>
+    <div>
       <p onClick={props.click}>
         I am {props.name} and I am {props.age} years old!
       </p>
       <p>{props.children}</p>
       <input type="text" onChange={props.changed} value={props.name}></input>
-    </StyledDiv>
+    </div>
   );
 };
 
