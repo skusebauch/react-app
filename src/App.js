b03e9bc6a133eaad1cc2e8d2321c3f0f048f1e91
Sven Kusebauch <sven.kusebauch@gmail.com>
2020-08-29 18:48:32 +0200

    stage before css modules

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
 import React, { Component } from "react";
-import styled from "styled-components";
 
 import "./App.css";
 import Person from "./Person/Person";
 
-const StyledButton = styled.button`
-  background-color: ${(props) => (props.alt ? "red" : "green")};
-  color: white;
-  font: inherit;
-  border: 1px solid blue;
-  padding: 8px;
-  cursor: pointer;
-  transition: all 0.25s;
+//const StyledButton = styled.button`
+//  background-color: ${(props) => (props.alt ? "red" : "green")};
+//  color: white;
+//  font: inherit;
+//  border: 1px solid blue;
+//  padding: 8px;
+//  cursor: pointer;
+//  transition: all 0.25s;
 
-  &:hover {
-    background-color: ${(props) => (props.alt ? "#FC7979" : "#BFFC79")};
-    color: black;
-  },
-`;
+//  &:hover {
+//    background-color: ${(props) => (props.alt ? "#FC7979" : "#BFFC79")};
+//    color: black;
+//  },
+//`;
 
 class App extends Component {
   state = {
@@ -92,12 +91,13 @@ class App extends Component {
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p className={classes.join(" ")}>This is really working!</p>
-        <StyledButton
+        <button
+          className="button"
           alt={this.state.showPersons}
           onClick={this.togglePersonsHandler}
         >
           Toggle Persons
-        </StyledButton>
+        </button>
         {persons}
       </div>
     );
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,19 +1,18 @@
 import React from "react";
-import styled from "styled-components";
 //import "./Person.css";
 
-const StyledDiv = styled.div`
-  width: 70%;
-  margin: 16px auto;
-  border: 1px solid #eee;
-  box-shadow: 0 2px 3px #ccc;
-  padding: 16px;
-  text-align: center;
+//const StyledDiv = styled.div`
+//  width: 70%;
+//  margin: 16px auto;
+//  border: 1px solid #eee;
+//  box-shadow: 0 2px 3px #ccc;
+//  padding: 16px;
+//  text-align: center;
 
-  @media only screen and (min-width: 500px) {
-    width: 450px;
-  }
-`;
+//  @media only screen and (min-width: 500px) {
+//    width: 450px;
+//  }
+//`;
 
 const person = (props) => {
   //const style = {
