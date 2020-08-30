f8a6056cd13b4cd43d8ff4deea89d84cdb052b95
Sven Kusebauch <sven.kusebauch@gmail.com>
2020-08-30 23:53:51 +0200

    added prop types

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,16 +1,26 @@
 import React from "react";
+import PropTypes from "prop-types";
 import classes from "./Person.css";
+import Aux from "../../../hoc/Aux";
+import WithClass from "../../../hoc/WithClass";
 
 const person = (props) => {
   return (
-    <div className={classes.Person}>
+    <WithClass classes={classes.Person}>
       <p onClick={props.click}>
         I am {props.name} and I am {props.age} years old!
       </p>
       <p>{props.children}</p>
       <input type="text" onChange={props.changed} value={props.name}></input>
-    </div>
+    </WithClass>
   );
+};
+
+person.propTypes = {
+  click: PropTypes.func,
+  name: PropTypes.string,
+  age: PropTypes.number,
+  changes: PropTypes.func,
 };
 
 export default person;
